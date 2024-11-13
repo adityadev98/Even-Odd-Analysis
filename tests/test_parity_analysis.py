26e83c980962f74c8dc1b1c546ec20d423bdbc73
adityadev98 <aditya.dev43@gmail.com>
2024-11-12 20:59:30 -0500

    Unit tests for True positive Top and Unknown states

--- tests/test_parity_analysis.py
+++ tests/test_parity_analysis.py
@@ -4,17 +4,34 @@ from src.parity_analysis import analyze_cfg, Parity
 
 class TestParityAnalysis(unittest.TestCase):
 
-    def test_simple_even(self):
-        code = "x = 2"
+    def test_unknown_input(self):
+        code = "x = input()"
         cfg = build_cfg(code)
         result = analyze_cfg(cfg)
-        self.assertEqual(result[2]['x'], Parity.EVEN)
+        self.assertEqual(result[2]['x'], Parity.UNKNOWN)
 
-    def test_simple_odd(self):
-        code = "x = 3"
+    def test_unknown_result(self):
+        code = """
+x = input()  # Unknown input
+y = x + 1    # Still unknown
+"""
         cfg = build_cfg(code)
         result = analyze_cfg(cfg)
-        self.assertEqual(result[2]['x'], Parity.ODD)
+        self.assertEqual(result[3]['y'], Parity.UNKNOWN)
+
+    def test_top_result_from_if(self):
+        code = """
+x = input()  # Unknown input
+if x > 0:
+    y = 2    # Even
+else:
+    y = 3    # Odd
+"""
+        cfg = build_cfg(code)
+        result = analyze_cfg(cfg)
+        y_nodes = [node for node in result.keys() if 'y' in result[node]]
+        y_node = max(y_nodes)
+        self.assertEqual(result[y_node]['y'], Parity.TOP)
 
 if __name__ == '__main__':
     unittest.main()
